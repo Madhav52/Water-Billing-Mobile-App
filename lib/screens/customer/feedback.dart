1db2cd36abb3f6a58903720e648cb8381ce59f73
Bipin <madhavbelbase52@gmail.com>
2020-02-28 22:06:26 +0545

    feedback form created

---
+++ lib/screens/customer/feedback.dart
@@ -0,0 +1,294 @@
+
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_login_page_ui/screens/fragments/about_us.dart';
+import 'package:flutter_login_page_ui/screens/fragments/contact_us.dart';
+import 'package:flutter_login_page_ui/screens/fragments/home_fragment.dart';
+import 'package:flutter_login_page_ui/screens/fragments/about_app.dart';
+import 'package:flutter_login_page_ui/screens/fragments/our_mission.dart';
+import 'package:flutter_login_page_ui/screens/fragments/our_vision.dart';
+import 'package:flutter_login_page_ui/screens/fragments/rate_us.dart';
+import 'package:flutter_login_page_ui/screens/fragments/privacy_policy.dart';
+
+
+class DrawerItem {
+  String title;
+  IconData icon;
+  DrawerItem(this.title, this.icon);
+}
+
+class Feedback extends StatefulWidget {
+  final drawerItems = [
+    new DrawerItem("Home", Icons.home),
+    new DrawerItem("About Us", Icons.supervisor_account),
+    new DrawerItem("Our Mission", Icons.my_location),
+    new DrawerItem("Our Vision", Icons.wb_sunny),
+    // new DrawerItem("Setting", Icons.settings),
+    new DrawerItem("Contact us", Icons.contacts),
+    new DrawerItem("Rate Us", Icons.thumb_up),
+    new DrawerItem("Privacy Policy", Icons.verified_user),
+    new DrawerItem("About App", Icons.touch_app),
+    
+  ];
+
+  @override
+  _FeedbackState createState() => new _FeedbackState();
+}
+
+class _FeedbackState extends State<Feedback> {
+
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData media = MediaQuery.of(context);
+
+    final Size screenSize = media.size;
+              
+    var drawerOptions = <Widget>[];
+    for (var i = 0; i < widget.drawerItems.length; i++) {
+      var d = widget.drawerItems[i];
+      var listTile = new ListTile(
+        leading: new Icon(
+            d.icon,
+            color: Colors.deepPurple
+        ),
+        
+        title: new Text(
+            d.title,
+            style: new TextStyle(
+                color: Colors.deepPurple,
+                fontWeight: FontWeight.bold
+            )),
+            
+        selected: i == _selectedIndex,
+        onTap: () => _onSelectItem(i),
+      );
+      // actions: <Widget>[
+      //   IconButton(icon:Icon(Icons.notifications), onPressed: (){})        
+      // ];
+      drawerOptions.add(
+        new Column(
+          children: <Widget>[
+            listTile,
+            new Divider(
+              color: Colors.deepPurple,
+              height: 2.0,
+            )
+          ],
+        )
+      );
+      
+    }
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.drawerItems[_selectedIndex].title),
+        actions: <Widget>[
+          new IconButton(icon: Icon(Icons.notifications, color: Colors.white), onPressed: null),
+          
+        ],
+        elevation: defaultTargetPlatform== TargetPlatform.android?5.0:0.0,
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+                accountName: new Text("Madhav"),
+                accountEmail: new Text("Madhav@gmail.com"),
+                // phoneNumber: new Text("9865373625"),
+
+                currentAccountPicture: new CircleAvatar(
+                  maxRadius: 50.0,
+                  backgroundColor: Colors.transparent,
+                  // borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                  child: new Center(
+                    child: new Image.asset(
+                      "assets/madhav.jpg",
+                      height: 58.0,
+                      width: 58.0,
+                  ),)
+                 // backgroundImage: new Image.network(src),
+                ),
+
+            ),
+            new Column(
+              children: drawerOptions
+            ),
+          ],
+        ),
+      ),
+      body: new GestureDetector(
+        onTap: () {
+        // call this method here to hide soft keyboard
+        FocusScope.of(context).requestFocus(new FocusNode());
+        },
+        child: Container(
+        width: screenSize.width,
+        child: new ListView( 
+        children: <Widget>[
+          new Container(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              
+              children: <Widget>[
+                  new Container(
+                    margin: EdgeInsets.only(top:30),
+                    height:580.0,
+                    width: 350.0,
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        
+                      ),
+                      elevation: 10.0,
+                      child: Column(
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                       children: <Widget>[
+                         Container(
+                           padding: EdgeInsets.only(top: 30),
+                           child: Center(
+                           child: Text("Contact Us",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: "Open-Sans",
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                              letterSpacing: 1.5)
+                            ),
+                           ),
+                          ),
+                          Container(
+                           padding: EdgeInsets.fromLTRB(30,20,30,20),
+                           child: Center(
+                           child: Text("Hi! If you have any queries or confusion regarding this app, just feel free to fill the form and send to us.",
+                           textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: "Roboto",
+                              fontSize: 18,
+                              
+                              letterSpacing: 1.0)
+                            ),
+                           ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                            child: TextField(
+                              keyboardType: TextInputType.text,
+                              autofocus: false,
+                              decoration: InputDecoration(
+                                icon: Icon(Icons.person),
+                                labelText: "Full Name*",
+                                hintText: 'Enter your full name.'
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                            child: TextField(
+                              keyboardType: TextInputType.number,
+                              autofocus: false,
+                              decoration: InputDecoration(
+                                icon: Icon(Icons.phonelink_ring),
+                                labelText: "Phone Number*",
+                                hintText: 'Enter Your Valid Phone No.'
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                            child: TextField(
+                              keyboardType: TextInputType.text,
+                              autofocus: false,
+                              decoration: InputDecoration(
+                                icon: Icon(Icons.subject),
+                                labelText: "Subject*",
+                                hintText: 'Subject For Contacting Us.'
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                            child: TextField(
+                              maxLines: 3,
+                              keyboardType: TextInputType.text,
+                              autofocus: false,
+                              decoration: InputDecoration(
+                                icon: Icon(Icons.message),
+                                labelText: "Message",
+                                hintText: 'Write Your Message Here.'
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(top:30),
+                            child: Center(
+                              child: ButtonTheme(
+                                minWidth: 120.0,
+                                height: 55.0,
+                                child: RaisedButton(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: new BorderRadius.circular(30.0),
+                                ),
+                                color: Colors.deepPurpleAccent,
+                                  onPressed: () {},
+                                  child: const Text(
+                                    'Submit',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontFamily: "Open-Sans",
+                                      color: Colors.white
+                                      )
+                                    
+                                  ),
+                                ),
+                              ),
+                            ),
+                            
+                          ),
+                       ], 
+                      )
+                    ),
+                  ),
+              ]
+              ),
+            ),
+        ]
+        ),
+      ),
+      ),
+      // _setDrawerItemWidget(_selectedIndex)
+    );
+  }
+  _setDrawerItemWidget(int pos) {
+    switch (pos) {
+      case 0:
+        return new HomeFragment();
+      case 1:
+        return new AboutUs();
+      case 2:
+        return new OurMission();
+      case 3:
+        return new OurVision();
+      case 4:
+        return new ContactUs();
+      case 5:
+        return new PrivacyPolicy();
+      case 6:
+        return new RateUs();
+      case 7:
+        return new AboutApp();
+
+      default:
+        return new Text("Error");
+    }
+  }
+
+  _onSelectItem(int index) {
+    setState(() => _selectedIndex = index);
+    Navigator.of(context).pop(); // close the drawer
+  }
+
+
+}
