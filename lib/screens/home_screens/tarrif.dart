1206e3607c31df0eb0e514ee039dd36b6197b198
Bipin <madhavbelbase52@gmail.com>
2020-03-02 23:16:23 +0545

    login button added

--- lib/screens/home_screens/tarrif.dart
+++ lib/screens/home_screens/tarrif.dart
@@ -1,5 +1,4 @@
 
-import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_login_page_ui/screens/fragments/about_us.dart';
@@ -10,9 +9,10 @@ import 'package:flutter_login_page_ui/screens/fragments/our_mission.dart';
 import 'package:flutter_login_page_ui/screens/fragments/our_vision.dart';
 import 'package:flutter_login_page_ui/screens/fragments/rate_us.dart';
 import 'package:flutter_login_page_ui/screens/fragments/privacy_policy.dart';
-import 'package:carousel_pro/carousel_pro.dart';
-import 'package:flutter_login_page_ui/screens/home_screens/tarrif.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mdi/mdi.dart';
+
+import '../login.dart';
+
 
 
 class DrawerItem {
@@ -68,9 +68,6 @@ class _TarrifState extends State<Tarrif> {
         selected: i == _selectedIndex,
         onTap: () => _onSelectItem(i),
       );
-      // actions: <Widget>[
-      //   IconButton(icon:Icon(Icons.notifications), onPressed: (){})        
-      // ];
       drawerOptions.add(
         new Column(
           children: <Widget>[
@@ -84,26 +81,28 @@ class _TarrifState extends State<Tarrif> {
       );
       
     }
-    
-    Widget image_slider_carousel = Container(
-      height: 250,
-      child: new Carousel(
-        boxFit: BoxFit.fill,
-        images: [
-          new AssetImage("assets/water.png"),
-          new AssetImage("assets/water.png"),
-          new AssetImage("assets/water.png"),
-          
-        ],
-      ),
-    );
-
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(widget.drawerItems[_selectedIndex].title),
         actions: <Widget>[
-          new IconButton(icon: Icon(Icons.notifications, color: Colors.white), onPressed: null),
-          
+          FlatButton(
+                onPressed: () => {
+                   Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()), )
+                },
+                
+                splashColor: Colors.yellow,
+                color: Colors.deepPurple,
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Mdi.login, color: Colors.white,),
+                    Text("Login", style: TextStyle(color: Colors.white, fontSize: 18))
+                    
+                  ],
+                ),
+              ),
         ],
         elevation: defaultTargetPlatform== TargetPlatform.android?5.0:0.0,
       ),
