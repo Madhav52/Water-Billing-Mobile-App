cbf44d4e233f36327d0e7f7e4e7664b86acdae5c
Bipin <madhavbelbase52@gmail.com>
2020-03-27 17:12:50 +0545

    billing history page optimized

--- lib/screens/billing/billing_history.dart
+++ lib/screens/billing/billing_history.dart
@@ -1,119 +1,57 @@
 import 'package:flutter_login_page_ui/screens/billing/billing_statement.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_login_page_ui/screens/fragments/about_app.dart';
-import 'package:flutter_login_page_ui/screens/fragments/about_us.dart';
-import 'package:flutter_login_page_ui/screens/fragments/contact_us.dart';
-import 'package:flutter_login_page_ui/screens/fragments/our_mission.dart';
-import 'package:flutter_login_page_ui/screens/fragments/our_vision.dart';
-import 'package:flutter_login_page_ui/screens/fragments/privacy_policy.dart';
-import 'package:flutter_login_page_ui/screens/fragments/rate_us.dart';
 import 'package:mdi/mdi.dart';
-
+import '../../api.dart';
+import '../../drawer.dart';
 import '../home.dart';
 
-class DrawerItem {
-  String title;
-  IconData icon;
-  DrawerItem(this.title, this.icon);
-}
-
 class BillingHistory extends StatefulWidget {
-  final drawerItems = [
-    new DrawerItem("Home", Icons.home),
-    new DrawerItem("About Us", Icons.supervisor_account),
-    new DrawerItem("Our Mission", Icons.my_location),
-    new DrawerItem("Our Vision", Icons.wb_sunny),
-    // new DrawerItem("Setting", Icons.settings),
-    new DrawerItem("Contact us", Icons.contacts),
-    new DrawerItem("Rate Us", Icons.thumb_up),
-    new DrawerItem("Privacy Policy", Icons.verified_user),
-    new DrawerItem("About App", Icons.touch_app),
-    
-  ];
-
+  
   @override
   _BillingHistoryState createState() => new _BillingHistoryState();
 }
 
 class _BillingHistoryState extends State<BillingHistory> {
 
-  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     MediaQueryData media = MediaQuery.of(context);
-
     final Size screenSize = media.size;
-              
-    var drawerOptions = <Widget>[];
-    for (var i = 0; i < widget.drawerItems.length; i++) {
-      var d = widget.drawerItems[i];
-      var listTile = new ListTile(
-        leading: new Icon(
-            d.icon,
-            color: Colors.deepPurple
-        ),
-        
-        title: new Text(
-            d.title,
-            style: new TextStyle(
-                color: Colors.deepPurple,
-                fontWeight: FontWeight.bold
-            )),
-            
-        selected: i == _selectedIndex,
-        onTap: () => _onSelectItem(i),
-      );
-      drawerOptions.add(
-        new Column(
-          children: <Widget>[
-            listTile,
-            new Divider(
-              color: Colors.deepPurple,
-              height: 2.0,
-            )
-          ],
-        )
-      );
-      
-    } return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.drawerItems[_selectedIndex].title),
+     
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Text('Billing History'),
         actions: <Widget>[
-          new IconButton(icon: Icon(Icons.notifications, color: Colors.white), onPressed: null),
-          
-        ],
-        elevation: defaultTargetPlatform== TargetPlatform.android?5.0:0.0,
-      ),
-      drawer: new Drawer(
-        child: new ListView(
-          children: <Widget>[
-            new UserAccountsDrawerHeader(
-                accountName: new Text("Madhav"),
-                accountEmail: new Text("Madhav@gmail.com"),
-                // phoneNumber: new Text("9865373625"),
-
-                currentAccountPicture: new CircleAvatar(
-                  maxRadius: 50.0,
-                  backgroundColor: Colors.transparent,
-                  // borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                  child: new Center(
-                    child: new Image.asset(
-                      "assets/madhav.jpg",
-                      height: 58.0,
-                      width: 58.0,
-                  ),)
-                 // backgroundImage: new Image.network(src),
+          new IconButton(
+              icon: Icon(Icons.notifications, color: Colors.white),
+              onPressed: null),
+          FlatButton(
+                onPressed: ()  {
+                  CallApi().logout();
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()), );
+                 
+                },
+                splashColor: Colors.yellow,
+                color: Colors.deepPurple,
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(right:5),
+                        child: Icon(Mdi.login,  color: Colors.white),
+                    ),
+                    Text("Logout", style: TextStyle(color: Colors.white, fontSize: 20))
+                    
+                  ],
                 ),
-
-            ),
-            new Column(
-              children: drawerOptions
-            ),
-          ],
-        ),
+              ),
+        ],
+        elevation: 5,
       ),
+      drawer: SideDrawer(),
       body: new Container(
         width: screenSize.width,
         height: screenSize.height,
@@ -380,37 +318,6 @@ class _BillingHistoryState extends State<BillingHistory> {
         ]
         ),
       ),
-      // _setDrawerItemWidget(_selectedIndex)
     );
   }
-  _setDrawerItemWidget(int pos) {
-    switch (pos) {
-      case 0:
-        return new HomeScreen();
-      case 1:
-        return new AboutUs();
-      case 2:
-        return new OurMission();
-      case 3:
-        return new OurVision();
-      case 4:
-        return new ContactUs();
-      case 5:
-        return new PrivacyPolicy();
-      case 6:
-        return new RateUs();
-      case 7:
-        return new AboutApp();
-
-      default:
-        return new Text("Error");
-    }
-  }
-
-  _onSelectItem(int index) {
-    setState(() => _selectedIndex = index);
-    Navigator.of(context).pop(); // close the drawer
-  }
-
-
 }
