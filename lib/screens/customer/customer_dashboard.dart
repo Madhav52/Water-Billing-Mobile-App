452f0bba2e82c810546b6b23c53986b21adc6de8
Bipin <madhavbelbase52@gmail.com>
2020-03-27 16:30:50 +0545

    customer dashboard page optimized

--- lib/screens/customer/customer_dashboard.dart
+++ lib/screens/customer/customer_dashboard.dart
@@ -6,45 +6,19 @@ import 'package:flutter_login_page_ui/screens/customer/complaints.dart';
 import 'package:flutter_login_page_ui/screens/customer/feedback.dart';
 import 'package:flutter_login_page_ui/screens/customer/make_payment.dart';
 import 'package:flutter_login_page_ui/screens/customer/view_profile.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter_login_page_ui/screens/root_view.dart';
 import 'package:mdi/mdi.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_login_page_ui/screens/fragments/about_app.dart';
-import 'package:flutter_login_page_ui/screens/fragments/about_us.dart';
-import 'package:flutter_login_page_ui/screens/fragments/contact_us.dart';
-import 'package:flutter_login_page_ui/screens/fragments/our_mission.dart';
-import 'package:flutter_login_page_ui/screens/fragments/our_vision.dart';
-import 'package:flutter_login_page_ui/screens/fragments/privacy_policy.dart';
-import 'package:flutter_login_page_ui/screens/fragments/rate_us.dart';
-
+import '../../api.dart';
+import '../../drawer.dart';
 import '../home.dart';
 
-class DrawerItem {
-  String title;
-  IconData icon;
-  DrawerItem(this.title, this.icon);
-}
-
 class CustomerDashboard extends StatefulWidget {
-  final drawerItems = [
-    new DrawerItem("Home", Icons.home),
-    new DrawerItem("About Us", Icons.supervisor_account),
-    new DrawerItem("Our Mission", Icons.my_location),
-    new DrawerItem("Our Vision", Icons.wb_sunny),
-    // new DrawerItem("Setting", Icons.settings),
-    new DrawerItem("Contact us", Icons.contacts),
-    new DrawerItem("Rate Us", Icons.thumb_up),
-    new DrawerItem("Privacy Policy", Icons.verified_user),
-    new DrawerItem("About App", Icons.touch_app),
-  ];
 
   @override
   _CustomerDashboardState createState() => new _CustomerDashboardState();
 }
 
 class _CustomerDashboardState extends State<CustomerDashboard> {
-  int _selectedIndex = 0;
     var userData;
 
   void initState() {
@@ -64,81 +38,41 @@ class _CustomerDashboardState extends State<CustomerDashboard> {
 
   @override
   Widget build(BuildContext context) {
-    MediaQueryData media = MediaQuery.of(context);
-    final Size screenSize = media.size;
-
-    var drawerOptions = <Widget>[];
-    for (var i = 0; i < widget.drawerItems.length; i++) {
-      var d = widget.drawerItems[i];
-      var listTile = new ListTile(
-        leading: new Icon(d.icon, color: Colors.deepPurple),
-        title: new Text(d.title,
-            style: new TextStyle(
-                color: Colors.deepPurple, fontWeight: FontWeight.bold)),
-        selected: i == _selectedIndex,
-        onTap: () => _onSelectItem(i),
-      );
-      drawerOptions.add(new Column(
-        children: <Widget>[
-          listTile,
-          new Divider(
-            color: Colors.deepPurple,
-            height: 2.0,
-          )
-        ],
-      ));
-    }
+    // MediaQueryData media = MediaQuery.of(context);
+    // final Size screenSize = media.size;
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.drawerItems[_selectedIndex].title),
+      appBar: AppBar(
+        title: new Text('Customer Home'),
         actions: <Widget>[
           new IconButton(
               icon: Icon(Icons.notifications, color: Colors.white),
               onPressed: null),
           FlatButton(
-            onPressed: logout,
-            splashColor: Colors.yellow,
-            color: Colors.deepPurple,
-            padding: EdgeInsets.only(right: 10.0, left:10),
-            child: Row(
-              children: <Widget>[
-                Padding(
+                onPressed: ()  {
+                  CallApi().logout();
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()), );
+                 
+                },
+                splashColor: Colors.yellow,
+                color: Colors.deepPurple,
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
                       padding: EdgeInsets.only(right:5),
-                        child: Icon(Mdi.logout,  color: Colors.white),
+                        child: Icon(Mdi.login,  color: Colors.white),
                     ),
-                Text("Logout",
-                    style: TextStyle(color: Colors.white, fontSize: 20))
-              ],
-            ),
-          ),
+                    Text("Logout", style: TextStyle(color: Colors.white, fontSize: 20))
+                    
+                  ],
+                ),
+              ),
         ],
-        elevation: defaultTargetPlatform == TargetPlatform.android ? 5.0 : 0.0,
+        elevation: 5,
       ),
-      drawer: new Drawer(
-        child: new ListView(
-          children: <Widget>[
-            new UserAccountsDrawerHeader(
-              accountName: new Text("Madhav"),
-              accountEmail: new Text("Madhav@gmail.com"),
-              currentAccountPicture: new CircleAvatar(
-                  maxRadius: 50.0,
-                  backgroundColor: Colors.transparent,
-                  child: new Center(
-                    child: new Image.asset(
-                      "assets/madhav.jpg",
-                      height: 58.0,
-                      width: 58.0,
-                    ),
-                  )
-                  // backgroundImage: new Image.network(src),
-                  ),
-            ),
-            new Column(children: drawerOptions),
-          ],
-        ),
-      ),
-      
-      
+      drawer: SideDrawer(),
       body: new SingleChildScrollView(
         
         child: new Column(
@@ -155,20 +89,20 @@ class _CustomerDashboardState extends State<CustomerDashboard> {
                 
                   
                   child: Padding(
-                  padding: EdgeInsets.only(top: 10, left:10),
+                  padding: EdgeInsets.only(top: 30, left:10),
                   child: Text(
                     ("Namaste! ") + (userData!= null ? '${userData['name']}' : 'user') ,
                       style: TextStyle(
                           color: Colors.black,
                           fontFamily: "Open-Sans",
-                          fontWeight: FontWeight.w700,
+                          fontWeight: FontWeight.w600,
                           fontSize: 30,
                           letterSpacing: 1.5)),
                 ),
               ),
               new Container(
-                margin: EdgeInsets.only(top: 100, left: 60),
-                height: 90.0,
+                margin: EdgeInsets.only(top: 110, left: 60),
+                height: 70.0,
                 width: 300.0,
                 child: Center(
                   child: Card(
@@ -180,8 +114,8 @@ class _CustomerDashboardState extends State<CustomerDashboard> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
                           Padding(
-                            padding: EdgeInsets.fromLTRB(30.0, 22, 30, 0),
-                            child: Text("Good Morning!",
+                            padding: EdgeInsets.fromLTRB(30.0, 13, 20, 0),
+                            child: Text("Good "+ greeting(),
                                 style: TextStyle(
                                     color: Colors.black,
                                     fontFamily: "Open-Sans",
@@ -430,48 +364,21 @@ class _CustomerDashboardState extends State<CustomerDashboard> {
           ),
         ]),
       ),
-      // _setDrawerItemWidget(_selectedIndex)
     );
   }
-
-  _setDrawerItemWidget(int pos) {
-    switch (pos) {
-      case 0:
-        return new HomeScreen();
-      case 1:
-        return new AboutUs();
-      case 2:
-        return new OurMission();
-      case 3:
-        return new OurVision();
-      case 4:
-        return new ContactUs();
-      case 5:
-        return new PrivacyPolicy();
-      case 6:
-        return new RateUs();
-      case 7:
-        return new AboutApp();
-
-      default:
-        return new Text("Error");
+  String greeting(){
+    var hour  = DateTime.now().hour;
+    if (hour < 12){
+      return 'Morning';
     }
-  }
-
-  _onSelectItem(int index) {
-    setState(() => _selectedIndex = index);
-    Navigator.of(context).pop(); // close the drawer
-  }
-
-  void logout() async {
-    // logout from the server ...
-    SharedPreferences localStorage = await SharedPreferences.getInstance();
-    var lsData = localStorage.get('token');
-    if (lsData != null) {
-      SharedPreferences localStorage = await SharedPreferences.getInstance();
-      localStorage.remove('token');
-      Navigator.push(
-          context, new MaterialPageRoute(builder: (context) => RootView()));
+    if (hour < 17){
+      return 'Afternoon';
+    }
+    if (hour < 20){
+      return 'Evening';
+    }
+    else{
+      return 'Night';
     }
   }
 }
